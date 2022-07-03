218e304696f91191141492dae178774e17bbb6b5
dennyzain <abbasdenny24@gmail.com>
2022-07-03 15:20:37 +0700

    chore: add final-form and react-final-form

--- components/organisms/HomeInventory.js
+++ components/organisms/HomeInventory.js
@@ -1,24 +1,24 @@
-import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import Card from '@/components/molecules/Card';
 import ListPagination from '@/components/molecules/ListPagination';
 import { useSession } from 'next-auth/react';
 import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
 export default function HomeInventory({ data }) {
   const state = useSelector((state) => state.global);
   const { status } = useSession();
+  const router = useRouter();
+  const handleRedirect = () => {
+    status === 'unauthenticated'
+      ? toast.error('you must be login for access this url!')
+      : router.push('/inventories/add');
+  };
   return (
     <main className="layout">
-        status === 'unauthenticated' ? toast.error('you must be login for access this url!')
-      <Link
-        href={'/inventories/add'}
-        onClick={() =>
-        
-        }
-      >
-        <button className="bg-blue-600 text-white p-2 my-2 rounded-lg">Tambah Barang</button>
-      </Link>
+      <button onClick={handleRedirect} className="bg-blue-600 text-white p-2 my-2 rounded-lg">
+        Tambah Barang
+      </button>
       <h1 className="text-xl font-bold">List Barang Inventory</h1>
       <hr />
       <div className="grid grid-rows-5 space-y-4 my-2">
